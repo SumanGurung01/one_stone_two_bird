612de461f57ceabd0fa4bd781ee7401f407c94d7
Suman Gurung <suman_201900250@smit.smu.edu.in>
2023-01-16 11:47:21 +0530

    add two new levels

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 import pygame
 import random
 
+# slingblade width : 80
+# slingblade height : 100
+
+# stone.width : 20
+# stone.height : 20
+
+# bird.width : 50 
+# bird.height :  50
+
 pygame.init()
+level=1
 
 # CONSTANTS
 WIN_HEIGHT,WIN_WIDTH = 600,600
@@ -23,65 +33,126 @@ class Slingblade():
         if direction == "right":
             self.x += 5 
 
+class Stone():
+    def __init__(self , x , y ):
+        self.x = x
+        self.y = y
+        self.width = 20
+        self.height = 20
+        self.image = pygame.image.load("assets/stone.png")  
+        self.release = 0
+        self.count = 1
+    def move(self):
+        self.y -= 5 
+
 class Bird():
     def __init__(self , x , y ):
         self.x = x
         self.y = y
         self.width = 50
         self.height = 50
-        self.dirX = 1
+        self.dirX = random.choice([-1,1])
         self.dirY = 1  
-        self.image = pygame.image.load("assets/bird1.png")  
+        if self.dirX < 0 : 
+            self.image = pygame.image.load("assets/bird4.png")
+        else:
+            self.image = pygame.image.load("assets/bird1.png")  
         self.limit = y 
     def move(self):
-        self.x+= self.dirX*3
-        
+        self.y += self.dirY * 1
         if self.y >= self.limit + 5:
             self.dirY *= -1 
         if self.y <= self.limit - 5:
             self.dirY *= -1 
-        
-        self.y += self.dirY * 1
 
+        self.x+= self.dirX*2
         if self.x+50 >= WIN_WIDTH:
             self.dirX *= -1
             self.image = pygame.image.load("assets/bird4.png")
         if self.x <= 0:
             self.dirX *= -1
             self.image = pygame.image.load("assets/bird1.png")
-        
-        
 
-def draw(win , slingblade , bird):
+def handle_slingblade_stone_movement(keys , slingblade , stone):
+    if keys[pygame.K_a] and slingblade.x - 5 >= 0:
+        slingblade.move(direction = "left")
+        if not stone.release: 
+            stone.x = slingblade.x+30
+    if keys[pygame.K_d] and slingblade.x + slingblade.width + 5<= WIN_WIDTH:
+        slingblade.move(direction = "right")
+        if not stone.release: 
+            stone.x = slingblade.x+30
+    if keys[pygame.K_SPACE]:
+        stone.release=1
+        stone.count-=1
+    if stone.release:
+        stone.move()
+
+def handle_collision(birds , stone):
+    global level
+    for i , bird in enumerate(birds , start=0):
+        if stone.x+10 >= bird.x and stone.x+10 <= bird.x+50:
+            if stone.y <= bird.y+50 and stone.y+20 >= bird.y:
+                birds.pop(i)
+                level += 1
+
+def draw(win , slingblade , birds , stone):
     background = pygame.transform.scale( pygame.image.load("assets/bg.jpg"), (600,600))
     win.blit(background , (0 , 0))
 
     slingblade_sprite = pygame.transform.scale( pygame.image.load("assets/slingblade.png"), (80,100))
-    win.blit(slingblade_sprite , (slingblade.x - 40, slingblade.y-100))
+    win.blit(slingblade_sprite , (slingblade.x , slingblade.y))
 
-    bird_sprite = pygame.transform.scale( bird.image, (bird.width,bird.height))
-    win.blit(bird_sprite , (bird.x, bird.y))
+    for bird in birds:
+        bird_sprite = pygame.transform.scale( bird.image, (bird.width,bird.height))
+        win.blit(bird_sprite , (bird.x, bird.y))
+        bird.move()
 
-    bird.move()
+    stone_sprite = pygame.transform.scale(stone.image, (stone.width,stone.height))
+    win.blit(stone_sprite , (stone.x,stone.y))
     
     pygame.display.update()
 
 def main():
     run = True 
     clock = pygame.time.Clock()
-
-    # birds = []
-
-    slingblade = Slingblade(WIN_WIDTH//2 , WIN_HEIGHT)
-
-    bird = Bird(0,100)
-
+    
+    global level
     while run:
-        clock.tick(60) 
-        draw(WIN,slingblade,bird)       
+        if level==1:
+            print("level 1")
+            birds = []
+            slingblade = Slingblade(WIN_WIDTH//2 - 40 , WIN_HEIGHT - 100)
+            birds.append(Bird(random.randrange(50,550),100))
+            stone = Stone(slingblade.x + 30 , slingblade.y + 50)  
+        if level==2:
+            print("level 2")
+            birds = []
+            slingblade = Slingblade(WIN_WIDTH//2 - 40 , WIN_HEIGHT - 100)
+            for i in range(1,3):
+                bird = Bird(random.randrange(50,550),i*60)
+                birds.append(bird)
+            stone = Stone(slingblade.x + 30 , slingblade.y + 50)     
+        
+        isrunning = True
+
+        while isrunning:
+            clock.tick(60) 
+            draw(WIN,slingblade,birds,stone)
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+            if len(birds) == 0:
+                isrunning = False
+            keys = pygame.key.get_pressed() 
+            handle_slingblade_stone_movement(keys , slingblade , stone)
+            handle_collision(birds , stone) 
+        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                 pygame.quit()
+                pygame.quit()
+        
+       
 
 if __name__ == "__main__":
     main()
